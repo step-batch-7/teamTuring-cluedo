45cde79298808d8b34a7f71900b6673d5f825363
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-03-06 16:53:18 +0530

    |#4| neha/anil | extracted app.js from server.js.     pulled out express app from server and then exported it to server. This resolves the test not calling done problem.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('./public'));
-app.listen(process.env.PORT || 4000);
+const app = require('./lib/app');
+// eslint-disable-next-line no-process-env
+const port = process.env.PORT || 4000;
+app.listen(port);
+// eslint-disable-next-line no-console
+console.log(`server started listening on port : ${port}`);

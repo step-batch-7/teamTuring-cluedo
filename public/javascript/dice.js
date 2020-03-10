ecf677024446ae10b62b9f5abb9ff2011f004e09
ayush <58028488+desibabua@users.noreply.github.com>
2020-03-10 18:21:43 +0530

    |#32|ayush/naveen|added animatin to dice

--- public/javascript/dice.js
+++ public/javascript/dice.js
@@ -1,4 +1,18 @@
+const faces = [
+  'translateZ(-2.5rem) rotate3d(0, 0, 0, 0deg);',
+  'translateZ(-2.5rem) rotate3d(0, 1, 0, -90deg);',
+  'translateZ(-2.5rem) rotate3d(1, 0, 0, -90deg);',
+  'translateZ(-2.5rem) rotate3d(1, 0, 0, 90deg);',
+  'translateZ(-2.5rem) rotate3d(0, 1, 0, 90deg);',
+  'translateZ(-2.5rem) rotate3d(0, 1, 0, 180deg);'
+];
 const updateDiceFace = function({ values }) {
-  document.querySelector('#dice1').src = `./images/dice/dice${values[0]}.png`;
-  document.querySelector('#dice2').src = `./images/dice/dice${values[1]}.png`;
+  const dices = Array.from(document.querySelectorAll('div[id^="cube"]'));
+  dices.forEach((dice, index) => {
+    dice.className = `show${values[index]}`;
+    setTimeout(() => {
+      dice.className = '';
+      dice.setAttribute('style', `transform :${faces[values[index] - 1]}`);
+    }, 2200);
+  });
 };

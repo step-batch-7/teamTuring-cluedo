193d573fe4864ca7054983a43425771de4365bd1
Shankara <=>
2020-03-13 17:07:32 +0530

    |#3|shankar|added a travis.yml for CI

--- test/testDistributionUtils.js
+++ test/testDistributionUtils.js
@@ -1,13 +1,14 @@
 const assert = require('assert');
 const sinon = require('sinon');
-const { shuffle, selectConfidential } = require('../lib/distributionUtils');
+let shuffle = require('../lib/distributionUtils');
 
-describe('shuffle', function() {
+describe('shuffle', function () {
   const fake = () => 0.2;
   before(() => {
     sinon.replace(Math, 'random', fake);
+    shuffle = () => 10;
   });
-  it('should shuffle a list of cards', function() {
+  it.only('should shuffle a list of cards', function () {
     const list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
     const actual = shuffle(list);
     assert.deepStrictEqual(actual, [
@@ -28,12 +29,12 @@ describe('shuffle', function() {
   });
 });
 
-describe('selectConfidential', function() {
+describe('selectConfidential', function () {
   const fake = () => 0;
   before(() => {
     sinon.replace(Math, 'random', fake);
   });
-  it('should separate the confidential cards from total', function() {
+  it('should separate the confidential cards from total', function () {
     const rooms = ['r1', 'r2', 'r3', 'r4'];
     const weapons = ['w1', 'w2', 'w3', 'w4'];
     const characters = ['c1', 'c2', 'c3', 'c4'];

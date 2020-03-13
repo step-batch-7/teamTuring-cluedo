5b69b680e8ba527ed27ea040b801fcc2d24a4ddf
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-03-13 11:29:07 +0530

    |#33|anil/naveen| added test for Player class methods getPosition, getMessage and modified tests of move and changeMessage also

--- test/testPlayer.js
+++ test/testPlayer.js
@@ -2,14 +2,31 @@ const assert = require('assert');
 const { Player } = require('../lib/player');
 
 describe('Player', function() {
-  it('should move player to the given location', function() {
-    const player1 = new Player(0, 'name', 'character', '1_1');
-    player1.move('1_2');
-    assert.deepStrictEqual(player1.getPosition(), '1_2');
+  describe('move', () => {
+    it('should move player to the given location', function() {
+      const player1 = new Player(0, 'shankar', 'character', '1_1');
+      player1.move('1_2');
+      assert.deepStrictEqual(player1.getPosition(), '1_2');
+    });
   });
-  it('should change player message with the given message', function() {
-    const player1 = new Player(0, 'name', 'character', '1_1');
-    player1.changeMessage('hello sir');
-    assert.deepStrictEqual(player1.getMessage(), 'hello sir');
+  describe('changeMessage', () => {
+    it('should change player message with the given message', function() {
+      const player1 = new Player(0, 'ayush', 'character', '1_1');
+      player1.changeMessage('welcome to cluedo');
+      assert.deepStrictEqual(player1.getMessage(), 'welcome to cluedo');
+    });
+  });
+  describe('getPosition', () => {
+    it('Should give current position of the player', () => {
+      const player = new Player(0, 'anil', 'scarlet', '8_25');
+      assert.strictEqual(player.getPosition(), '8_25');
+    });
+  });
+  describe('getMessage', () => {
+    it('Should give the current message of the player', () => {
+      const player = new Player(0, 'anil', 'scarlet', '8_25');
+      player.changeMessage('welcome to cluedo');
+      assert.strictEqual(player.getMessage(), 'welcome to cluedo');
+    });
   });
 });

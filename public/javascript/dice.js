389e489f799f49cbcaaf6b0400bf9fede1b7539b
Shankara <=>
2020-03-14 10:29:22 +0530

    |#18|trinangkur/shankar|Added a action tab

--- public/javascript/dice.js
+++ public/javascript/dice.js
@@ -6,7 +6,7 @@ const faces = [
   'translateZ(-2.5rem) rotate3d(0, 1, 0, 90deg);',
   'translateZ(-2.5rem) rotate3d(0, 1, 0, 180deg);'
 ];
-const updateDiceFace = function({ values }) {
+const updateDiceFace = function ({ values }) {
   const dices = Array.from(document.querySelectorAll('div[id^="cube"]'));
   dices.forEach((dice, index) => {
     dice.className = `show${values[index]}`;
@@ -18,7 +18,7 @@ const updateDiceFace = function({ values }) {
   setTimeout(() => getPossiblePositions(), 2200);
 };
 
-const updateDiceAndPossibilities = function({ diceValues, possiblePositions }) {
+const updateDiceAndPossibilities = function ({ diceValues, possiblePositions }) {
   const dices = Array.from(document.querySelectorAll('div[id^="cube"]'));
   dices.forEach((dice, index) => {
     dice.className = '';
